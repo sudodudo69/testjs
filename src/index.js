7024f5082fb0d3145ba7e62a736a15851a52ae33
sudodudo69 <115925229+sudodudo69@users.noreply.github.com>
2024-01-10 00:28:54 -0800

    Update index.js
    d

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 /// <reference types="@fastly/js-compute" />
 
 import { Router } from "@fastly/expressly";
+import axios from 'axios';
 
 const router = new Router();
+
+// Proxy requests to Binance API and expose result at /api/btc
+router.get('/api/btc', async (req, res) => {
+  try {
+    // Make a request to the Binance API
+    const binanceResponse = await axios.get('https://api.binance.com/api/v3/depth', {
+      params: {
+        limit: 10,
+        symbol: 'BTCUSDT',
+      },
+    });
+
+    // Forward Binance API response to the client
+    res.json(binanceResponse.data);
+  } catch (error) {
+    console.error('Error fetching Binance API:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 // Use middleware to set a header
 router.use((req, res) => {
   res.set("x-powered-by", "expressly");
 });
-
-// GET 200 response
-router.get('/', (req, res) => {
-  res.sendStatus(200); // "OK"
-});
-
-// POST simple message
-router.post("/submit", async (req, res) => {
-  let body = await req.text();
-  res.send(`You posted: "${body}"`)
-})
-
-// 404/405 response for everything else
 
 router.listen();
